5e03cbd77ceeb6c197ebeb72a4e0f1d9455b10c8
Your Name <you@example.com>
2018-01-22 14:47:16 +0600

    fixed

--- block_chain_test.py
+++ block_chain_test.py
@@ -5,12 +5,6 @@ import requests
 import time
 
 class BlockChainTestCase(unittest.TestCase):
-
-    def assertTxInBlock(self, result, jvtToken):
-        self.assertIn("hash",  result)
-        status = utils.txstatus(result['hash'], jvtToken)
-        self.assertEqual(status['errmsg'], "")
-        self.assertGreater(len(status['blockid']), 0)
         
     def get_name_and_code(self):
         name = utils.generate_random_name()
@@ -33,20 +27,6 @@ class BlockChainTestCase(unittest.TestCase):
         data = {'Wallet': '', 'Value': code, 'Conditions': """ContractConditions(`MainCondition`)"""}
         resp = utils.call_contract("NewContract", data, self.data["jvtToken"])
         return name
-
-
-    def get_count_contract(self):
-        resp = requests.get(config.config["url"] + '/list/contracts', headers={"Authorization": self.data["jvtToken"]})
-        result = resp.json()
-        return result["count"]
-    
-    def change_contract(self, id, code):
-        data = {'Value': code, "Conditions": """ContractConditions(`MainCondition`)""", "Id": id}
-        sign_res = utils.prepare_tx('EditContract', self.data['jvtToken'], data)
-        data.update(sign_res)
-        resp = requests.post(config.config["url"] + '/contract/EditContract', data=data, headers={"Authorization": self.data["jvtToken"]})
-        result = resp.json()
-        self.assertTxInBlock(result, self.data["jvtToken"])
     
     def test_block_chain(self):
         db1 = "aplafront"
@@ -61,7 +41,7 @@ class BlockChainTestCase(unittest.TestCase):
         while i < ts_count:
             contName = self.create_contract()
             i = i + 1
-			time.sleep(1)
+            time.sleep(1)
         time.sleep(15)
         self.assertTrue(utils.get_count_records_block_chain(host, db1, login, pas), "There isn't 30 records in block_chain1")
         self.assertTrue(utils.get_count_records_block_chain(host, db2, login, pas), "There isn't 30 records in block_chain2")
