96ae5d3f67e22515d61fd7e62ecbbf126b6d0ef9
Konkina Elena <lfreze@gmail.com>
2018-02-01 13:30:09 +0600

    Added universal path to config

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import json
+import os
 
 
 def readMainConfig():
-	with open('.\config.json', 'r') as f:
+	path = os.path.join(os.getcwd(), "config.json")
+	with open(path, 'r') as f:
 		data = f.read()
 	return json.loads(data)
 
 
 def getNodeConfig():
-	with open('.\hostConfig.json', 'r') as f:
+	path = os.path.join(os.getcwd(), "hostConfig.json")
+	with open(path, 'r') as f:
 		data = f.read()
 	return json.loads(data)
 
@@ -16,7 +19,7 @@ def getNodeConfig():
 def readFixtures(type):
 	path = ""
 	if type == "contracts":
-		path = '.\\fixtures\contracts.json'
+		path = os.path.join(os.getcwd(), "fixtures", "contracts.json")
 	with open(path, 'r') as f:
 		data = f.read()
 	return json.loads(data)
