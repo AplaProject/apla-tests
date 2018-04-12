767f75d61c2fe3e8d7680c0278fe5ec32557ba00
arkuz <arkuzka@mail.ru>
2018-04-12 21:42:17 +0300

    test_page_sys_var_ecosystem_name

--- protypo_tests.py
+++ protypo_tests.py
@@ -235,6 +235,12 @@ class PrototipoTestCase(unittest.TestCase):
         content = self.check_page(contract["code"])
         self.assertEqual(content["tree"][1]["children"][0]["text"], contract["content"],
                          "ecosystem_id is not equal " + contract["content"] + ". Content = " + str(content["tree"]))
+
+    def test_page_sys_var_ecosystem_name(self):
+        contract = self.pages["sys_var_ecosystem_name"]
+        content = self.check_page(contract["code"])
+        self.assertEqual(content["tree"][1]["children"][0]["text"], contract["content"],
+                         "ecosystem_id is not equal " + contract["content"] + ". Content = " + str(content["tree"]))
         
 if __name__ == '__main__':
     unittest.main()
