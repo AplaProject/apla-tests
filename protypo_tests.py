1ee6e50f87a07c87cd91eab418c8cc0f5c0a2fec
arkuz <arkuzka@mail.ru>
2018-04-12 21:35:33 +0300

    Added test_page_sys_var_roleID

--- protypo_tests.py
+++ protypo_tests.py
@@ -223,6 +223,12 @@ class PrototipoTestCase(unittest.TestCase):
         content = self.check_page(contract["code"])
         self.assertEqual(content["tree"][1]["children"][0]["text"], contract["content"],
                          "isMobile is not equal " + contract["content"] + ". Content = " + str(content["tree"]))
+
+    def test_page_sys_var_roleID(self):
+        contract = self.pages["sys_var_roleID"]
+        content = self.check_page(contract["code"])
+        self.assertEqual(content["tree"][1]["children"][0]["text"], contract["content"],
+                         "role_id is not equal " + contract["content"] + ". Content = " + str(content["tree"]))
         
 if __name__ == '__main__':
     unittest.main()
