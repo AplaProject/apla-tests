b6b34b27fd359e6ccb2c802b76f98e0d88d714a8
arkuz <arkuzka@mail.ru>
2018-04-13 12:33:18 +0300

    Added test_page_sys_var_key_id

--- protypo_tests.py
+++ protypo_tests.py
@@ -250,6 +250,14 @@ class PrototipoTestCase(unittest.TestCase):
         content = self.check_page(contract["code"])
         self.assertEqual(content["tree"][1]["children"][0]["text"], contract["content"],
                          "ecosystem_name is not equal " + contract["content"] + ". Content = " + str(content["tree"]))
+
+    def test_page_sys_var_key_id(self):
+        content = self.check_page("Span(EcosysParam(founder_account))")
+        founderAcc = content["tree"][0]["children"][0]["text"]
+        contract = self.pages["sys_var_keyID"]
+        content = self.check_page(contract["code"])
+        self.assertEqual(content["tree"][1]["children"][0]["text"], founderAcc,
+                        "key_id is not equal " + contract["content"] + ". Content = " + str(content["tree"]))
         
 if __name__ == '__main__':
     unittest.main()
