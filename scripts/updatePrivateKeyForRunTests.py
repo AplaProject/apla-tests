0ac5c37a2e532383103180d64f273a9e7b9e5e59
arkuz <arkuzka@mail.ru>
2018-04-25 11:46:01 +0300

    Changed for start 1 node

--- scripts/updatePrivateKeyForRunTests.py
+++ scripts/updatePrivateKeyForRunTests.py
@@ -10,19 +10,29 @@ args = parser.parse_args()
 with open(os.path.join(args.privateKeyPath, 'PrivateKey'), 'r') as f:
 	privKey1 = f.read()
 
-config = os.path.join(args.configPath, 'config.json')
+config = os.path.join(args.configPath, 'hostConfig.json')
 with open(config) as fconf:
 	lines = fconf.readlines()
 
-# Update private keys in config
-del lines[2]
-lines.insert(2, " \"private_key\": \""+privKey1+"\",\n")
+# Update private keys in hostConfig
+del lines[4]
+lines.insert(4, "\t\t\"private_key\": \""+privKey1+"\",\n")
+del lines[15]
+lines.insert(15, "\t\t\"private_key\": \""+privKey1+"\",\n")
 
-del lines[5]
-lines.insert(5, " \"dbName\": \""+args.dbName+"\",\n")
+# Update db name in hostConfig
+del lines[7]
+lines.insert(7, "\t\t\"dbName\": \""+args.dbName+"\",\n")
+del lines[18]
+lines.insert(18, "\t\t\"dbName\": \""+args.dbName+"\",\n")
+
+# Update url 2 in hostConfig
+del lines[14]
+lines.insert(14, lines[3])
+
 with open(config, 'w') as fconf:
 	fconf.write(''.join(lines))
 
-print("PrivateKey is updated!")
+print("hostConfig is updated!")
 
 
