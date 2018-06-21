3a3a57211d2bb1ffa5c2ff0287f442a60db1090a
arkuz <arkuzka@mail.ru>
2018-06-21 13:50:20 +0300

    Changed test_new_contract_exists_name

--- system_contracts_tests.py
+++ system_contracts_tests.py
@@ -231,10 +231,8 @@ class SystemContractsTestCase(unittest.TestCase):
         res = self.call("NewContract", data)
         self.assertGreater(int(res), 0, "BlockId is not generated: " + res)
         ans = self.call("NewContract", data)
-        #msg = "Contract or function " + name + " exists"
-        #self.assertEqual(ans, msg, "Incorrect message: " + ans)
-        self.assertGreater(int(res), 0, "BlockId is not generated: " + res)
-
+        msg = "Contract " + name + " already exists"
+        self.assertEqual(ans, msg, "Incorrect message: " + ans)
 
     def test_new_contract_without_name(self):
         code = "contract {data { }    conditions {    }    action {    }    }"
