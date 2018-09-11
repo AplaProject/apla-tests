f22bbf0b2d66ab011d0aa42017b6a19c30413ec8
Lena Konkina <lfreze@gmail.com>
2018-09-11 14:01:10 +0600

    Added api.json and jsonToList

--- model/help_actions.py
+++ model/help_actions.py
@@ -14,3 +14,14 @@ class HelpActions(object):
         with open(path, 'r', encoding='UTF-8') as f:
             data = f.read()
         return json.loads(data)
+    
+    def jsonToList(js):
+        fullList = []
+        list = []
+        tup = ()
+        for i in js:
+            for element in i:
+                list.append(i[element])
+            tup = tuple(list)
+            fullList.append(tup)
+        return fullList
