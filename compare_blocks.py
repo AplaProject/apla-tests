15b4bd790a4b850fde9172b246241cdff07bbd13
Lena Konkina <lfreze@gmail.com>
2018-09-19 14:05:23 +0600

    Fixed CompareBlocks

--- compare_blocks.py
+++ compare_blocks.py
@@ -1,18 +1,18 @@
 import unittest
 import time
 
-from libs.actions import Actions
-from libs.tools import Tools
+from libs import actions
+from libs import tools
 
 #TODO bugs
 
 class CompareBlocks(unittest.TestCase):
 
     def get_load_time(self, url, token, maxBlock):
-        testConf = Tools.read_config("test")
+        testConf = tools.read_config("test")
         sec = 1
         while sec < testConf["wait_upating_node"]:
-            maxBlockId1 = Actions.get_max_block_id(url, token)
+            maxBlockId1 = actions.get_max_block_id(url, token)
             if maxBlockId1 == maxBlock:
                 print("Time: ", sec)
                 return {"time": sec, "blocks": maxBlockId1}
@@ -21,15 +21,17 @@ class CompareBlocks(unittest.TestCase):
         return {"time": testConf["wait_upating_node"] + 1, "blocks": maxBlockId1}
     
     def test_compare_blocks(self):
-        nodeConf = Tools.read_config("nodes")
+        nodeConf = tools.read_config("nodes")
+        testConf = tools.read_config("test")
         time.sleep(30)
-        data2 = Actions.login(nodeConf['2']['url'], nodeConf['1']['private_key'], 0)
-        maxBlockId2 = Actions.get_max_block_id(nodeConf["2"]["url"], data2["jwtToken"])
-        data1 = Actions.login(nodeConf['1']['url'], nodeConf['1']['private_key'], 0)
-        res = self.get_load_time(conf["1"]["url"], data1["jwtToken"], maxBlockId2)
+        data2 = actions.login(nodeConf['2']['url'], nodeConf['1']['private_key'], 0)
+        maxBlockId2 = actions.get_max_block_id(nodeConf["2"]["url"], data2["jwtToken"])
+        data1 = actions.login(nodeConf['1']['url'], nodeConf['1']['private_key'], 0)
+        res = self.get_load_time(nodeConf["1"]["url"], data1["jwtToken"], maxBlockId2)
         msg = "All " + str(maxBlockId2) +\
          " blocks doesn't load in time. Last loaded block is " + str(res['blocks'])
-        self.assertLess(res['time'], testConf["wait_upating_node"], msg)
+        unit = unittest.TestCase()
+        unit.assertLess(res['time'], testConf["wait_upating_node"], msg)
 
 
 if __name__ == '__main__':
