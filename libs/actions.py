25f14e7d8b573caeb2208457318f1d22794ef2b0
Lena Konkina <lfreze@gmail.com>
2018-09-19 12:09:30 +0600

    Took out methods from classes in libraries.

--- libs/actions.py
+++ libs/actions.py
@@ -9,208 +9,200 @@ from collections import Counter
 from genesis_blockchain_tools.crypto import sign
 from genesis_blockchain_tools.crypto import get_public_key
 
+def get_uid(url):
+    resp = requests.get(url + '/getuid')
+    result = resp.json()
+    return result['token'], result['uid']
 
-class Actions(object):
+def login(url, prKey, role=0, ecosystem=1):
+    token, uid = get_uid(url)
+    signature = sign(prKey, "LOGIN" + uid)
+    pubkey = get_public_key(prKey)
+    fullToken = 'Bearer ' + token
+    data = {'pubkey': pubkey, 'signature': signature, "role_id": role, "ecosystem": ecosystem}
+    head = {'Authorization': fullToken}
+    resp = requests.post(url + '/login', params=data, headers=head)
+    res = resp.json()
+    result = {}
+    result["uid"] = uid
+    result["timeToken"] = res["refresh"]
+    result["jwtToken"] = 'Bearer ' + res["token"]
+    result["pubkey"] = pubkey
+    result["address"] = res["address"]
+    result["key_id"] = res["key_id"]
+    return result
 
-    def get_uid(url):
-        resp = requests.get(url + '/getuid')
-        result = resp.json()
-        return result['token'], result['uid']
+def prepare_tx(url, prKey, entity, jvtToken, data):
+    heads = {'Authorization': jvtToken}
+    resp = requests.post(url + '/prepare/' + entity, data=data, headers=heads)
+    result = resp.json()
+    signature = sign(prKey, result['forsign'])
+    return {"time": result['time'], "signature": signature, "reqID": result['request_id']}
 
-    def login(url, prKey, role=0, ecosystem=1):
-        token, uid = Actions.get_uid(url)
-        signature = sign(prKey, "LOGIN" + uid)
-        pubkey = get_public_key(prKey)
-        fullToken = 'Bearer ' + token
-        data = {'pubkey': pubkey, 'signature': signature, "role_id": role, "ecosystem": ecosystem}
-        head = {'Authorization': fullToken}
-        resp = requests.post(url + '/login', params=data, headers=head)
-        res = resp.json()
-        result = {}
-        result["uid"] = uid
-        result["timeToken"] = res["refresh"]
-        result["jwtToken"] = 'Bearer ' + res["token"]
-        result["pubkey"] = pubkey
-        result["address"] = res["address"]
-        result["key_id"] = res["key_id"]
-        return result
+def prepare_tx_with_files(url, prKey, entity, jvtToken, data, files):
+    heads = {'Authorization': jvtToken}
+    resp = requests.post(url + '/prepare/' + entity,
+                         data=data, headers=heads, files=files)
+    result = resp.json()
+    signature = sign(prKey, result['forsign'])
+    return {"time": result['time'], "signature": signature, "reqID": result['request_id']}
 
-    def prepare_tx(url, prKey, entity, jvtToken, data):
-        heads = {'Authorization': jvtToken}
-        resp = requests.post(url + '/prepare/' + entity, data=data, headers=heads)
-        result = resp.json()
-        signature = sign(prKey, result['forsign'])
-        return {"time": result['time'], "signature": signature, "reqID": result['request_id']}
+def call_contract(url, prKey, name, data, jvtToken):
+    sign = prepare_tx(url, prKey, name, jvtToken, data)
+    dataContract = {"time": sign['time'], "signature": sign["signature"]}
+    urlEnd = url + '/contract/' + sign["reqID"]
+    resp = requests.post(urlEnd, data=dataContract, headers={"Authorization": jvtToken})
+    result = resp.json()
+    return result
 
-    def prepare_tx_with_files(url, prKey, entity, jvtToken, data, files):
-        heads = {'Authorization': jvtToken}
-        resp = requests.post(url + '/prepare/' + entity,
-                             data=data, headers=heads, files=files)
-        result = resp.json()
-        signature = sign(prKey, result['forsign'])
-        return {"time": result['time'], "signature": signature, "reqID": result['request_id']}
+def prepare_multi_tx(url, prKey, entity, jvtToken, data):
+    urlToCont = url + '/prepareMultiple/'
+    heads = {'Authorization': jvtToken}
+    request = {"token_ecosystem": "", "max_sum": "","payover": "",
+               "signed_by": "", "contracts": data}
+    resp = requests.post(urlToCont, data={"data": json.dumps(request)}, headers=heads)
+    result = resp.json()
+    forsigns = result['forsign']
+    signatures = [sign(prKey, forsign) for forsign in forsigns]
+    return {"time": result['time'], "signatures": signatures, "reqID": result['request_id']}
 
-    def call_contract(url, prKey, name, data, jvtToken):
-        sign = Actions.prepare_tx(url, prKey, name, jvtToken, data)
-        dataContract = {"time": sign['time'], "signature": sign["signature"]}
-        urlEnd = url + '/contract/' + sign["reqID"]
-        resp = requests.post(urlEnd, data=dataContract, headers={"Authorization": jvtToken})
-        result = resp.json()
-        return result
+def call_multi_contract(url, prKey, name, data, jvtToken):
+    sign = prepare_multi_tx(url, prKey, name, jvtToken, data)
+    dataContract = {"time": sign['time'], "signatures": sign["signatures"]}
+    urlEnd = url + '/contractMultiple/' + sign["reqID"]
+    resp = requests.post(urlEnd, data={"data": json.dumps(dataContract)},
+                         headers={"Authorization": jvtToken})
+    result = resp.json()
+    return result
 
-    def prepare_multi_tx(url, prKey, entity, jvtToken, data):
-        urlToCont = url + '/prepareMultiple/'
-        heads = {'Authorization': jvtToken}
-        request = {"token_ecosystem": "",
-                   "max_sum": "",
-                   "payover": "",
-                   "signed_by": "",
-                   "contracts": data}
-        resp = requests.post(urlToCont, data={"data": json.dumps(request)}, headers=heads)
-        result = resp.json()
-        forsigns = result['forsign']
-        signatures = [sign(prKey, forsign) for forsign in forsigns]
-        return {"time": result['time'], "signatures": signatures, "reqID": result['request_id']}
+def call_contract_with_files(url, prKey, name, data, files, jvtToken):
+    sign = prepare_tx_with_files(url, prKey, name, jvtToken, data, files)
+    dataContract = {"time": sign['time'], "signature": sign["signature"]}
+    urlEnd = url + '/contract/' + sign["reqID"]
+    resp = requests.post(urlEnd, data=dataContract, headers={"Authorization": jvtToken})
+    result = resp.json()
+    return result
 
-    def call_multi_contract(url, prKey, name, data, jvtToken):
-        sign = Actions.prepare_multi_tx(url, prKey, name, jvtToken, data)
-        dataContract = {"time": sign['time'], "signatures": sign["signatures"]}
-        urlEnd = url + '/contractMultiple/' + sign["reqID"]
-        resp = requests.post(urlEnd, data={"data": json.dumps(dataContract)}, headers={"Authorization": jvtToken})
-        result = resp.json()
-        return result
-
-    def call_contract_with_files(url, prKey, name, data, files, jvtToken):
-        sign = Actions.prepare_tx_with_files(url, prKey, name, jvtToken, data, files)
-        dataContract = {"time": sign['time'], "signature": sign["signature"]}
-        urlEnd = url + '/contract/' + sign["reqID"]
-        resp = requests.post(urlEnd, data=dataContract,
-                             headers={"Authorization": jvtToken})
-        result = resp.json()
-        return result
-
-    def tx_status(url, sleepTime, hsh, jvtToken):
-        sec = 0
-        urlEnd = url + '/txstatus/' + hsh
-        while sec < sleepTime:
-            time.sleep(1)
-            resp = requests.get(urlEnd, headers={'Authorization': jvtToken})
-            jresp = resp.json()
-            print(jresp)
-            status = {}
-            if (len(jresp['blockid']) > 0 and 'errmsg' not in json.dumps(jresp)) or ('errmsg' in json.dumps(jresp)):
-                print("if")
-                break
-            else:
-                print("else")
-                sec = sec + 1
-                print(sec)   
-        print("status", jresp)
-        print("blockid", jresp['blockid'])
-        if 'errmsg' not in jresp and jresp['blockid'] == '':
-            return {"blockid": None, "result": None,
-                "error": None}     
-        if 'errmsg' not in jresp and int(jresp['blockid']) > 0:
-            return {"blockid": int(jresp['blockid']), "result": jresp['result'],
-                "error": None}
+def tx_status(url, sleepTime, hsh, jvtToken):
+    sec = 0
+    urlEnd = url + '/txstatus/' + hsh
+    while sec < sleepTime:
+        time.sleep(1)
+        resp = requests.get(urlEnd, headers={'Authorization': jvtToken})
+        jresp = resp.json()
+        print(jresp)
+        status = {}
+        if (len(jresp['blockid']) > 0 and 'errmsg' not in json.dumps(jresp)) or ('errmsg' in json.dumps(jresp)):
+            print("if")
+            break
         else:
-            return {"blockid": 0, "error": jresp['errmsg']['error'], "result": None}
+            print("else")
+            sec = sec + 1
+            print(sec)   
+    print("status", jresp)
+    print("blockid", jresp['blockid'])
+    if 'errmsg' not in jresp and jresp['blockid'] == '':
+        return {"blockid": None, "result": None, "error": None}     
+    if 'errmsg' not in jresp and int(jresp['blockid']) > 0:
+        return {"blockid": int(jresp['blockid']), "result": jresp['result'], "error": None}
+    else:
+        return {"blockid": 0, "error": jresp['errmsg']['error'], "result": None}
 
 
-    def tx_status_multi(url, sleepTime, hshs, jvtToken):
-        urlEnd = url + '/txstatusMultiple/'
-        allTxInBlocks = False
-        sec = 0
-        while sec < sleepTime:
-            time.sleep(1)
-            resp = requests.post(urlEnd, params={"data": json.dumps({"hashes": hshs})},
-                                 headers={'Authorization': jvtToken})
-            jresp = resp.json()["results"]
-            for status in jresp.values():
-                if (len(status['blockid']) > 0 and 'errmsg' not in json.dumps(status)):
-                    allTxInBlocks = True
-                else:
-                    allTxInBlocks = False
-            if allTxInBlocks == True:
-                return jresp
+def tx_status_multi(url, sleepTime, hshs, jvtToken):
+    urlEnd = url + '/txstatusMultiple/'
+    allTxInBlocks = False
+    sec = 0
+    while sec < sleepTime:
+        time.sleep(1)
+        resp = requests.post(urlEnd, params={"data": json.dumps({"hashes": hshs})},
+                             headers={'Authorization': jvtToken})
+        jresp = resp.json()["results"]
+        for status in jresp.values():
+            if (len(status['blockid']) > 0 and 'errmsg' not in json.dumps(status)):
+                allTxInBlocks = True
             else:
-                sec = sec + 1
-        return jresp
+                allTxInBlocks = False
+        if allTxInBlocks == True:
+            return jresp
+        else:
+            sec = sec + 1
+    return jresp
 
         
-    def call_get_api(url, data, token):
-        resp = requests.get(url, data=data,  headers={"Authorization": token})
+def call_get_api(url, data, token):
+    resp = requests.get(url, data=data,  headers={"Authorization": token})
+    return resp.json()
+
+def call_get_api_with_full_response(url, data, token):
+    resp = requests.get(url, data=data,  headers={"Authorization": token})
+    return resp
+
+def call_post_api(url, data, token):
+    resp = requests.post(url, data=data,  headers={"Authorization": token})
+    if resp.status_code == 200:
         return resp.json()
+    else:
+        return None
 
-    def call_get_api_with_full_response(url, data, token):
-        resp = requests.get(url, data=data,  headers={"Authorization": token})
-        return resp
+def get_count(url, type, token):
+    endPoint = url + "/list/" + type
+    res = call_get_api(endPoint, "", token)
+    return res["count"]
 
-    def call_post_api(url, data, token):
-        resp = requests.post(url, data=data,  headers={"Authorization": token})
-        if resp.status_code == 200:
-            return resp.json()
-        else:
-            return None
+def get_list(url, type, token):
+    count = get_count(url, type, token)
+    endPoint = url + "/list/" + type + "?limit=" + count
+    res = call_get_api(endPoint, "", token)
+    return res
 
-    def get_count(url, type, token):
-        endPoint = url + "/list/" + type
-        res = Actions.call_get_api(endPoint, "", token)
-        return res["count"]
-
-    def get_list(url, type, token):
-        count = Actions.get_count(url, type, token)
-        endPoint = url + "/list/" + type + "?limit=" + count
-        res = self.call_get_api(endPoint, "", token)
-        return res
-
-    def get_contract_id(url, name, token):
-        endPoint = url + "/contract/" + name
-        res = Actions.call_get_api(endPoint, "", token)
-        return res["tableid"]
+def get_contract_id(url, name, token):
+    endPoint = url + "/contract/" + name
+    res = call_get_api(endPoint, "", token)
+    return res["tableid"]
 
 
-    def get_object_id(url, name, object, token):
-        id = None
-        endPoint = url + "/list/" + object + "?limit=1000"
-        res = Actions.call_get_api(endPoint, "", token)
-        for object in res["list"]:
-            if object["name"] == name:
-                id = object["id"]
-        return id
+def get_object_id(url, name, object, token):
+    id = None
+    endPoint = url + "/list/" + object + "?limit=1000"
+    res = call_get_api(endPoint, "", token)
+    for object in res["list"]:
+        if object["name"] == name:
+            id = object["id"]
+    return id
     
 
-    def is_contract_activated(url, name, token):
-        endPoint = url + "/contract/" + name
-        res = Actions.call_get_api(endPoint, "", token)
-        return res["active"]
+def is_contract_activated(url, name, token):
+    endPoint = url + "/contract/" + name
+    res = call_get_api(endPoint, "", token)
+    return res["active"]
 
-    def get_activated_wallet(url, name, token):
-        endPoint = url + "/contract/" + name
-        res = Actions.call_get_api(endPoint, "", token)
-        return res["walletid"]
+def get_activated_wallet(url, name, token):
+    endPoint = url + "/contract/" + name
+    res = call_get_api(endPoint, "", token)
+    return res["walletid"]
 
-    def get_parameter_id(url, name, token):
-        endPoint = url + "/ecosystemparam/" + name
-        res = Actions.call_get_api(endPoint, "", token)
-        return res["id"]
+def get_parameter_id(url, name, token):
+    endPoint = url + "/ecosystemparam/" + name
+    res = call_get_api(endPoint, "", token)
+    return res["id"]
 
-    def get_parameter_value(url, name, token):
-        endPoint = url + "/ecosystemparam/" + name
-        res = Actions.call_get_api(endPoint, "", token)
-        return res["value"]
+def get_parameter_value(url, name, token):
+    endPoint = url + "/ecosystemparam/" + name
+    res = call_get_api(endPoint, "", token)
+    return res["value"]
 
-    def get_content(url, type, name, lang, appId, token):
-        if(lang != ""):
-            data = {"lang": lang, "app_id": appId}
-        else:
-            data = ""
-        endPoint = url + "/content/" + type + "/" + name
-        res = Actions.call_post_api(endPoint, data, token)
-        return res
-
-    def get_max_block_id(url, token):
+def get_content(url, type, name, lang, appId, token):
+    if(lang != ""):
+        data = {"lang": lang, "app_id": appId}
+    else:
         data = ""
-        endPoint = url + "/maxblockid"
-        result = Actions.call_get_api(endPoint, data, token)
-        return result["max_block_id"]
+    endPoint = url + "/content/" + type + "/" + name
+    res = call_post_api(endPoint, data, token)
+    return res
+
+def get_max_block_id(url, token):
+    data = ""
+    endPoint = url + "/maxblockid"
+    result = call_get_api(endPoint, data, token)
+    return result["max_block_id"]
--- libs/tools.py
+++ libs/tools.py
@@ -3,71 +3,67 @@ import json
 import random
 import string
 
-from libs.actions import Actions
+import libs.actions
+
+def json_to_list(js):
+    fullList = []
+    list = []
+    tup = ()
+    for i in js:
+        for element in i:
+            list.append(i[element])
+        tup = tuple(list)
+        fullList.append(tup)
+    return fullList
 
 
-class Tools(object):
+def generate_random_name():
+    name = []
+    for _ in range(1, 30):
+        sym = random.choice(string.ascii_lowercase)
+        name.append(sym)
+    return "".join(name)
 
-    @staticmethod
-    def json_to_list(js):
-        fullList = []
-        list = []
-        tup = ()
-        for i in js:
-            for element in i:
-                list.append(i[element])
-            tup = tuple(list)
-            fullList.append(tup)
-        return fullList
+def generate_name_and_code(sourceCode):
+    name = "Cont_" + generate_random_name()
+    code = generate_code(sourceCode)
+    return code, name
 
-    @staticmethod
-    def generate_random_name():
-        name = []
-        for _ in range(1, 30):
-            sym = random.choice(string.ascii_lowercase)
-            name.append(sym)
-        return "".join(name)
+def generate_code(contractName, sourceCode):
+    if sourceCode == "":
+        sCode = """{data { }    conditions {    }    action {    }    }"""
+    else:
+        sCode = sourceCode
+    code = "contract " + contractName + sCode
+    return code
 
-    def generate_name_and_code(self, sourceCode):
-        name = "Cont_" + self.generate_random_name()
-        code = self.generate_code(sourceCode)
-        return code, name
 
-    def generate_code(contractName, sourceCode):
-        if sourceCode == "":
-            sCode = """{data { }    conditions {    }    action {    }    }"""
-        else:
-            sCode = sourceCode
-        code = "contract " + contractName + sCode
-        return code
+def read_config(type):
+    path = ""
+    if type == "main":
+        path = os.path.join(os.getcwd(), "config.json")
+    if type == "nodes":
+        path = os.path.join(os.getcwd(), "nodesConfig.json")
+    if type == "test":
+        path = os.path.join(os.getcwd(), "testConfig.json")
+    with open(path, 'r') as f:
+        data = f.read()
+    return json.loads(data)
 
-    @staticmethod
-    def read_config(type):
-        path = ""
-        if type == "main":
-            path = os.path.join(os.getcwd(), "config.json")
-        if type == "nodes":
-            path = os.path.join(os.getcwd(), "nodesConfig.json")
-        if type == "test":
-            path = os.path.join(os.getcwd(), "testConfig.json")
-        with open(path, 'r') as f:
-            data = f.read()
-        return json.loads(data)
 
-    @staticmethod
-    def read_fixtures(type):
-        path = ""
-        if type == "contracts":
-            path = os.path.join(os.getcwd(), "fixtures", "contracts.json")
-        if type == "pages":
-            path = os.path.join(os.getcwd(), "fixtures", "pages.json")
-        if type == "api":
-            path = os.path.join(os.getcwd(), "fixtures", "api.json")
-        if type == "keys":
-            path = os.path.join(os.getcwd(), "fixtures", "prKeys.json")
-        if type == "simvolio":
-            path = os.path.join(os.getcwd(), "fixtures", "simvolio.json")
-        with open(path, 'r', encoding='UTF-8') as f:
-            data = f.read()
-        return json.loads(data)
+def read_fixtures(type):
+    path = ""
+    if type == "contracts":
+        path = os.path.join(os.getcwd(), "fixtures", "contracts.json")
+    if type == "pages":
+        path = os.path.join(os.getcwd(), "fixtures", "pages.json")
+    if type == "api":
+        path = os.path.join(os.getcwd(), "fixtures", "api.json")
+    if type == "keys":
+        path = os.path.join(os.getcwd(), "fixtures", "prKeys.json")
+    if type == "simvolio":
+        path = os.path.join(os.getcwd(), "fixtures", "simvolio.json")
+    with open(path, 'r', encoding='UTF-8') as f:
+        data = f.read()
+    return json.loads(data)
 
