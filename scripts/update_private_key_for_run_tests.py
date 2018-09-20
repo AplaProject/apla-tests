5e63004da1a8313fc61f26c100ecb382088d2c5f
arkuz <arkuzka@mail.ru>
2018-09-20 23:21:55 +0300

    revert changes for update_private_key_for_run_tests.py script

--- scripts/update_private_key_for_run_tests.py
+++ scripts/update_private_key_for_run_tests.py
@@ -1,33 +1,39 @@
 import os
+import argparse
 import json
 
-def test_update_private_key_for_run_tests(request):
+parser = argparse.ArgumentParser()
+parser.add_argument('-privateKeyPath', default='D:\\genesis-go')
+parser.add_argument('-configPath', default='D:\\GitHub\\GenesisKernel\\genesis-tests\\')
+parser.add_argument('-dbName', default='gen1')
+args = parser.parse_args()
 
-	with open(os.path.join(request.config.getoption('--privateKeyPath'), 'PrivateKey'), 'r') as f:
-		privKey1 = f.read()
+with open(os.path.join(args.privateKeyPath, 'PrivateKey'), 'r') as f:
+    privKey1 = f.read()
 
-	file = os.path.join(request.config.getoption('--configPath'), "hostConfig.json")
-	with open(file, 'r') as f:
-		data = f.read()
-		lines = json.loads(data)
+file = os.path.join(args.configPath, "nodesConfig.json")
+with open(file, 'r') as f:
+    data = f.read()
+    lines = json.loads(data)
+    print(lines)
 
-	# Update private keys in config
-	lines["1"]["private_key"] = privKey1
-	lines["2"]["private_key"] = privKey1
-	lines["3"]["private_key"] = privKey1
+# Update private keys in config
+lines["1"]["private_key"] = privKey1
+lines["2"]["private_key"] = privKey1
+lines["3"]["private_key"] = privKey1
 
-	# Update url in config
-	lines["2"]["url"] = lines["1"]["url"]
-	lines["3"]["url"] = lines["1"]["url"]
+# Update url in config
+lines["2"]["url"] = lines["1"]["url"]
+lines["3"]["url"] = lines["1"]["url"]
 
-	# Update DB name in config
-	lines["1"]["dbName"] = request.config.getoption('--dbName1')
-	lines["2"]["dbName"] = request.config.getoption('--dbName1')
-	lines["3"]["dbName"] = request.config.getoption('--dbName1')
+# Update DB name in config
+lines["1"]["dbName"] = args.dbName
+lines["2"]["dbName"] = args.dbName
+lines["3"]["dbName"] = args.dbName
 
-	with open(file, 'w') as f:
-		json.dump(lines, f)
+print(lines)
 
-	print("hostConfig.json is updated!")
+with open(file, 'w') as f:
+    json.dump(lines, f, indent=4)
 
-
+print("hostConfig.json is updated!")
