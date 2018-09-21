82117ecdbc7848c7091d3bb8caa7860c9e78fd90
arkuz <arkuzka@mail.ru>
2018-09-22 00:11:59 +0300

    added api funcs

--- libs/api.py
+++ libs/api.py
@@ -1,18 +1,62 @@
-import random
-import string
-import requests
-import psycopg2
-import json
-import time
 from libs import actions
-
 from genesis_blockchain_tools.crypto import sign
 from genesis_blockchain_tools.crypto import get_public_key
+import requests
 
 
-#getuid
-#login
-#refresh
+def getuid(url):
+    endpoint = '/getuid'
+    url += endpoint
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = response.json()
+        token = result['token']
+        uid = result['uid']
+        return token, uid
+    else:
+        return None
+
+
+def login(url, private_key, role_id=0, ecosystem=1, expire=3600):
+    token, uid = getuid(url)
+    data_signature = 'LOGIN' + uid
+    signature = sign(private_key, data_signature)
+    pubkey = get_public_key(private_key)
+    full_token = 'Bearer ' + token
+    data = {
+        'role_id': role_id,
+        'ecosystem': ecosystem,
+        'expire': expire,
+        'pubkey': pubkey,
+        'signature': signature,
+        # key_id # not use with parameter 'pubkey'
+    }
+    head = {'Authorization': full_token}
+    endpoint = '/login'
+    url += endpoint
+    response = requests.post(url, data=data, headers=head)
+    res = response.json()
+    result = {
+        'uid': uid,
+        'timeToken': res['refresh'],
+        'jwtToken': 'Bearer ' + res['token'],
+        'pubkey': pubkey,
+        'address': res['address'],
+        'key_id': res['key_id'],
+    }
+    return result
+
+
+def refresh(url, token, expire=36000):
+    data = {
+        'token': token,
+        'expire': expire,
+    }
+    endpoint = '/refresh'
+    url += endpoint
+    response = requests.post(url, data=data)
+    result = response.json()
+    return result
 
 
 def version(url, token):
@@ -33,7 +77,6 @@ def ecosystemname(url, token, id=1):
     data = {'id': id}
     endpoint = '/ecosystemname'
     url += endpoint
-    print(url)
     return actions.call_get_api(url, data, token)
 
 
@@ -46,7 +89,6 @@ def appparams(url, token, appid, ecosystem=1, names=''):
         appid,
     )
     url += endpoint
-    print(data)
     return actions.call_get_api(url, data, token)
 
 
@@ -140,3 +182,123 @@ def history(url, token, name, id):
     )
     url += endpoint
     return actions.call_get_api(url, data, token)
+
+
+def interface(url, token, entity, name):
+    """ Parameter entity must be 'page', 'menu' or 'block' """
+    data = {}
+    endpoint = '/interface/{}/{}'.format(
+        entity,
+        name,
+    )
+    url += endpoint
+    return actions.call_get_api(url, data, token)
+
+
+def contracts(url, token, limit=25, offset=0):
+    data = {
+        'limit': limit,
+        'offset': offset,
+    }
+    endpoint = '/contracts'
+    url += endpoint
+    return actions.call_get_api(url, data, token)
+
+
+def contract(url, token, name):
+    data = {}
+    endpoint = '/contract/{}'.format(
+        name,
+    )
+    url += endpoint
+    return actions.call_get_api(url, data, token)
+
+
+# contract/{request_id}
+# contractMultiple/{request_id}
+# prepare/{name}
+# prepareMultiple
+# txstatus/{hash}
+# txstatusMultiple
+
+
+def content(url, token, entity, name, lang='en-US', app_id=1):
+    """ Parameter entity must be 'page' or 'menu' """
+    data = {
+        'lang': lang,
+        'app_id': app_id,
+    }
+    endpoint = '/content/{}/{}'.format(
+        entity,
+        name,
+    )
+    url += endpoint
+    return actions.call_post_api(url, data, token)
+
+
+def content_source(url, token, name):
+    data = {}
+    endpoint = 'content/source/{}'.format(
+        name,
+    )
+    url += endpoint
+    return actions.call_post_api(url, data, token)
+
+
+def content_hash(url, token, name):
+    data = {}
+    endpoint = 'content/hash/{}'.format(
+        name,
+    )
+    url += endpoint
+    return actions.call_post_api(url, data, token)
+
+
+def content_template(url, token, template, source=0):
+    data = {
+        'template': template,
+        'source': source,
+    }
+    endpoint = '/content'
+    url += endpoint
+    return actions.call_post_api(url, data, token)
+
+
+# node/{name} - only VDE
+
+
+def maxblockid(url, token):
+    data = {}
+    endpoint = '/maxblockid'
+    url += endpoint
+    return actions.call_get_api(url, data, token)
+
+
+def block(url, token, id):
+    data = {}
+    endpoint = '/block/{}'.format(
+        id,
+    )
+    url += endpoint
+    return actions.call_get_api(url, data, token)
+
+
+def avatar(url, token, member, ecosystem=1):
+    data = {}
+    endpoint = '/avatar/{}/{}'.format(
+        ecosystem,
+        member,
+    )
+    url += endpoint
+    return actions.call_get_api_with_full_response(url, data, token)
+
+
+def config_centrifugo(url, token):
+    data = {}
+    endpoint = '/config/centrifugo'
+    url += endpoint
+    return actions.call_get_api(url, data, token)
+
+
+if __name__ == 'main':
+    pass
