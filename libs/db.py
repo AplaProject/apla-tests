77ba41a0106348e14f8cc9fe118e4c23444df442
Lena Konkina <lfreze@gmail.com>
2018-10-18 14:29:11 +0600

    Fixed db lib

--- libs/db.py
+++ libs/db.py
@@ -3,7 +3,6 @@ import json
 
 
 from psycopg2.psycopg1 import cursor
-
 from libs import api
 
 #here
@@ -95,17 +94,7 @@ def get_import_app_data(db, member_id):
     return result[0][0]
 
 #block_chain compare_nodes
-def get_count_DB_objects(db, url, token):
-    tablesCount = {}
-    tables = get_ecosys_tables(url, token)
-    for table in tables:
-        tablesCount[table[2:]] = get_count_table(db, table)
-    return tablesCount
-
-#TODO for Elena
-
-#block_chain compare_nodes
-def get_count_DB_objects(db, url, token):
+def get_count_DB_objects(url, token):
     tables = {}
     list = api.tables(url, token)['list']
     for table in list:
@@ -190,7 +179,7 @@ def is_commission_in_history_new(db_host, db_name, login, password, id_from, id_
     connect = psycopg2.connect(host=db_host, dbname=db_name, user=login, password=password)
     cursor = connect.cursor()
     cursor.execute("select * from \"1_history\" WHERE sender_id=" + id_from +\
-				 " AND recipient_id=" + str(id_to) + " AND amount=" + str(summ))
+                 " AND recipient_id=" + str(id_to) + " AND amount=" + str(summ))
     rec = cursor.fetchall()
     if len(rec) > 0:
         return True
@@ -199,28 +188,28 @@ def is_commission_in_history_new(db_host, db_name, login, password, id_from, id_
 
 #rollback2
 def get_count_DB_objects_from_DB(dbHost, dbName, login, password):
-	tablesCount = {}
-	tables = get_ecosystem_tables_from_DB(dbHost, dbName, login, password)
-	for table in tables:
-		tablesCount[table[2:]] = get_count_table_from_DB(dbHost, dbName, login, password, table)
-	return tablesCount
+    tablesCount = {}
+    tables = get_ecosystem_tables_from_DB(dbHost, dbName, login, password)
+    for table in tables:
+        tablesCount[table[2:]] = get_count_table_from_DB(dbHost, dbName, login, password, table)
+    return tablesCount
 
 #rollback2
 def get_ecosystem_tables_from_DB(dbHost, dbName, login, password):
-	connect = psycopg2.connect(host=dbHost, dbname=dbName, user=login, password=password)
-	cursor = connect.cursor()
-	cursor.execute("select table_name from INFORMATION_SCHEMA.TABLES WHERE table_schema='public' AND table_name LIKE '1_%'")
-	tables = cursor.fetchall()
-	list = []
-	i = 0
-	while i < len(tables):
-		list.append(tables[i][0])
-		i = i + 1
-	return list
+    connect = psycopg2.connect(host=dbHost, dbname=dbName, user=login, password=password)
+    cursor = connect.cursor()
+    cursor.execute("select table_name from INFORMATION_SCHEMA.TABLES WHERE table_schema='public' AND table_name LIKE '1_%'")
+    tables = cursor.fetchall()
+    list = []
+    i = 0
+    while i < len(tables):
+        list.append(tables[i][0])
+        i = i + 1
+    return list
 
 #rollback2
 def get_count_table_from_DB(dbHost, dbName, login, password, table):
-	connect = psycopg2.connect(host=dbHost, dbname=dbName, user=login, password=password)
-	cursor = connect.cursor()
-	cursor.execute("SELECT count(*) FROM \"" + table + "\"")
-	return cursor.fetchall()[0][0]
+    connect = psycopg2.connect(host=dbHost, dbname=dbName, user=login, password=password)
+    cursor = connect.cursor()
+    cursor.execute("SELECT count(*) FROM \"" + table + "\"")
+    return cursor.fetchall()[0][0]
