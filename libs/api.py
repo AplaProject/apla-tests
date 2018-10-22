2f40d3d102bc14caa23daeb9b421a2af70469a46
arkuz <arkuzka@mail.ru>
2018-10-22 23:00:00 +0300

    added api ecosystems

--- libs/api.py
+++ libs/api.py
@@ -90,6 +90,13 @@ def ecosystemname(url, token, id=1):
     return call_get_api(url, data, token)
 
 
+def ecosystems(url, token):
+    data = {}
+    endpoint = '/ecosystems'
+    url += endpoint
+    return call_get_api(url, data, token)
+
+
 def appparams(url, token, appid, ecosystem=1, names=''):
     data = {}
     data['ecosystem'] = ecosystem
