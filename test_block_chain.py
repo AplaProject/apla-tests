11943964d31a4480087aab8ccef6eff67f1b1ce3
Lena Konkina <lfreze@gmail.com>
2018-10-24 10:01:36 +0600

    Deleted test for log.

--- test_block_chain.py
+++ test_block_chain.py
@@ -33,11 +33,6 @@ class TestBlockChain():
         res = actions.call_contract(url, pr_key, "NewMenu", data, self.token)
         return actions.get_count(url, "menu", self.token)
     
-    def test_ee(self):
-        res = db.get_table_hash(self.config1['db'], "1_pages")
-        print(res)
-        ans = db.get_table_hash(db22, "1_pages")
-    
     def test_block_chain(self):
         ts_count = 30
         i = 1
