28229146f18f23640477bda0ae5d090deae514d5
arkuz <arkuzka@mail.ru>
2018-11-02 16:12:22 +0300

    refactored contracts.py

--- libs/contract.py
+++ libs/contract.py
@@ -1,180 +1,283 @@
 import requests
-
 from genesis_blockchain_tools.crypto import get_public_key
 from libs import api, tools, actions
 
 
-def new_contract(url, pr_key, token, source='', name='', app=1, condition='true'):
-    if name == '':
+def new_contract(url, pr_key, token, source='',
+                 name='', app=1, condition='true', ecosystem=1):
+    if not name:
         code, name = tools.generate_name_and_code(source)
     else:
-        if source == '':
-            source = '{data { }    conditions {    }    action {    }    }'
+        if not source:
+            source = '{ data {} conditions {} action {} }'
         code = 'contract ' + name + source
-    data = {"Value": code, "ApplicationId": app, "Conditions": condition}
-    result = {"hash": actions.call_contract(url, pr_key, "NewContract", data, token),
-              "name": name, "code": code}
+    data = {"Value": code,
+            "ApplicationId": app,
+            "Conditions": condition}
+    res = actions.call_contract(url, pr_key, "@1NewContract",
+                                data,  token, ecosystem=ecosystem)
+    result = {"hash": res,
+              "name": name,
+              "code": code}
     return result
 
-def new_ecosystem(url, pr_key, token, name=''):
-    if name == '':
+
+def new_ecosystem(url, pr_key, token, name='', ecosystem=1):
+    if not name:
         name = "Ecosys_" + tools.generate_random_name()
-    else:
-        name = name
-    data = {"Name": "Ecosys_" + tools.generate_random_name()}
-    result = {"hash": actions.call_contract(url, pr_key, "NewEcosystem", data, token),
+    data = {"Name": name}
+    res = actions.call_contract(url, pr_key, "@1NewEcosystem",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               "name": name}
     return result
 
-def new_application(url, pr_key, token, name='', condition='true'):
-    if name == '':
+
+def new_application(url, pr_key, token, name='', condition='true', ecosystem=1):
+    if not name:
         name = "App" + tools.generate_random_name()
-    data = {"Name": name, "Conditions": condition}
-    result = {"hash": actions.call_contract(url, pr_key, "NewApplication", data, token),
+    data = {"Name": name,
+            "Conditions": condition}
+    res = actions.call_contract(url, pr_key, "@1NewApplication",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               "name": name}
     return result
 
-def new_table(url, pr_key, token, name='', columns='', perms='', appId=1):
-    if name == '':
+
+def new_table(url, pr_key, token, name='', columns='',
+              perms='', appId=1, ecosystem=1):
+    if not name:
         name = "Tab_" + tools.generate_random_name()
-    if perms == '':
-        perms = """{"insert": "false", "update": "true", "new_column": "true"}"""
-    if columns == '':
-        columns = """[{"name": "MyName", "type":"varchar", "index": "1", "conditions": "true"}]""" 
-    data = {"ApplicationId": appId, "Name": name,
-            "Columns": columns, "Permissions": perms}
-    result = {"hash": actions.call_contract(url, pr_key, "NewTable", data, token),
+    if not perms:
+        perms = '{"insert": "false", "update": "true", "new_column": "true"}'
+    if not columns:
+        columns = '[{"name": "MyName", "type":"varchar", "index": "1", "conditions": "true"}]'
+    data = {"ApplicationId": appId,
+            "Name": name,
+            "Columns": columns,
+            "Permissions": perms}
+    res = actions.call_contract(url, pr_key, "@1NewTable",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               "name": name}
     return result
 
-def new_column(url, pr_key, token, table, name='', type="varchar", up_perm="true",
-               read_perm="true"):
-    if name == '':
+
+def new_column(url, pr_key, token, table, name='', type="varchar",
+               up_perm="true", read_perm="true", ecosystem=1):
+    if not name:
         name = 'col_' + tools.generate_random_name()
-    data = {"TableName": table, "Name": name, "Type": type, "UpdatePerm": up_perm,
+    data = {"TableName": table,
+            "Name": name,
+            "Type": type,
+            "UpdatePerm": up_perm,
             "ReadPerm": read_perm}
-    result = {"hash": actions.call_contract(url, pr_key, "NewColumn", data, token),
+    res = actions.call_contract(url, pr_key, "@1NewColumn",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               'name': name}
     return result
 
-def edit_column(url, pr_key, token, table, column, up_perm="true",
-               read_perm="true"):
-    data_edit = {"TableName": table, "Name": column,
-                 "UpdatePerm": "false", "ReadPerm": "false"}
-    result = {"hash": actions.call_contract(url, pr_key, "EditColumn", data_edit, token),
+
+def edit_column(url, pr_key, token, table, column,
+                update_perm="true", read_perm="true", ecosystem=1):
+    data_edit = {"TableName": table,
+                 "Name": column,
+                 "UpdatePerm": update_perm,
+                 "ReadPerm": read_perm}
+    res = actions.call_contract(url, pr_key, "@1EditColumn",
+                                data_edit, token, ecosystem=ecosystem)
+    result = {"hash": res,
               'name': table}
     return result
 
-def edit_ecosystem_name(url, pr_key, token, id=1, name=''):
-    if name == '':
+
+def edit_ecosystem_name(url, pr_key, token, id=1, name='', ecosystem=1):
+    if not name:
         name = "Ecosys_" + tools.generate_random_name()
-    data = {"EcosystemID": id, "NewName": name}
-    result = {"hash": actions.call_contract(url, pr_key, "EditEcosystemName", data, token),
+    data = {"EcosystemID": id,
+            "NewName": name}
+    res = actions.call_contract(url, pr_key, "@1EditEcosystemName",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               'pr_key': pr_key}
     return result
 
-def new_lang(url, pr_key, token, name='', trans=''):
-    if name == '':
+
+def new_lang(url, pr_key, token, name='', trans='', ecosystem=1):
+    if not name:
         name = "Lang_" + tools.generate_random_name()
-    if trans == '':
-        trans = "{\"en\": \"false\", \"ru\" : \"true\"}"
-    data = {"Name": name, "Trans": trans}
-    result = {"hash": actions.call_contract(url, pr_key, "NewLang", data, token),
+    if not trans:
+        trans = '{"en": "false", "ru" : "true"}'
+    data = {"Name": name,
+            "Trans": trans}
+    res = actions.call_contract(url, pr_key, "@1NewLang",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               'name': name}
     return result
 
-def edit_contract(url, pr_key, token, id, new_source='', condition='true', wallet=''):
-    if new_source == '':
-        new_source = '{data { }    conditions {    }    action {    }    }'
+
+def edit_contract(url, pr_key, token, id, new_source='',
+                  condition='true', ecosystem=1):
+    name = actions.get_object_name(url, id, object, token)
+    if not new_source:
+        new_source = '{ data {} conditions {} action {} }'
     code = 'contract ' + name + new_source
-    if wallet == '':
-        wallet = "0005-2070-2000-0006-0200"
-    data = {"Id": id, "Value": code, "Conditions": condition, "WalletId": wallet}
-    result = {"hash": actions.call_contract(url, pr_key, "EditContract", data, token)}
+    data = {"Id": id,
+            "Value": code,
+            "Conditions": condition}
+    res = actions.call_contract(url, pr_key, "@1EditContract",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def edit_lang(url, pr_key, token, id, trans=''):
-    if trans == '':
-        trans = "{\"en\": \"false\", \"ru\" : \"true\"}"
-    data = {"Id": id, "Trans": trans}
-    result = {"hash": actions.call_contract(url, pr_key, "EditLang", data, token)}
+
+def edit_lang(url, pr_key, token, id, trans='', ecosystem=1):
+    if not trans:
+        trans = '{"en": "false", "ru": "true"}'
+    data = {"Id": id,
+            "Trans": trans}
+    res = actions.call_contract(url, pr_key, "@1EditLang",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def edit_application(url, pr_key, token, id, condition="true"):
-    data = {"ApplicationId": id, "Conditions": condition}
-    result = {"hash": actions.call_contract(url, pr_key, "EditApplication", data, token)}
+
+def edit_application(url, pr_key, token, id, condition="true", ecosystem=1):
+    data = {"ApplicationId": id,
+            "Conditions": condition}
+    res = actions.call_contract(url, pr_key, "@1EditApplication",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def del_application(url, pr_key, token, id, val):
-    data = {"ApplicationId": id, "Value": val}
-    result = {"hash": actions.call_contract(url, pr_key, "DelApplication", data, token)}
+
+def del_application(url, pr_key, token, id, val, ecosystem=1):
+    data = {"ApplicationId": id,
+            "Value": val}
+    res =  actions.call_contract(url, pr_key, "@1DelApplication",
+                                 data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def tokens_send(url, pr_key, token, wall, amount):
-    data = {"Recipient": wall, "Amount": amount}
-    result = {"hash": actions.call_contract(url, pr_key, "TokensSend", data, token)}
+
+def tokens_send(url, pr_key, token, wall, amount, ecosystem=1):
+    data = {"Recipient": wall,
+            "Amount": amount}
+    res = actions.call_contract(url, pr_key, "@1TokensSend",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def bind_wallet(url, pr_key, token, id, wallet=''):
-    if wallet == '':
+
+def bind_wallet(url, pr_key, token, id, wallet='', ecosystem=1):
+    if not wallet:
         data = {"Id": id}
     else:
-        {"Id": id, "WalletId": wallet}
-    result = {"hash": actions.call_contract(url, pr_key, "BindWallet", data, token)}
+        data = {"Id": id,
+                "WalletId": wallet}
+    res = actions.call_contract(url, pr_key, "@1BindWallet",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def unbind_wallet(url, pr_key, token, id):
+
+def unbind_wallet(url, pr_key, token, id, ecosystem=1):
     data = {"Id": id}
-    result = {"hash": actions.call_contract(url, pr_key, "UnbindWallet", data, token)}
+    res = actions.call_contract(url, pr_key, "@1UnbindWallet",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def new_parameter(url, pr_key, token, name='', val="test", condition="true"):
-    if name == '':
+
+def new_parameter(url, pr_key, token, name='', val="test",
+                  condition="true", ecosystem=1):
+    if not name:
         name = "Par_" + tools.generate_random_name()
-    data = {"Name": name, "Value": val,"Conditions": condition}
-    result = {"hash": actions.call_contract(url, pr_key, "NewParameter", data, token),
+    data = {"Name": name,
+            "Value": val,
+            "Conditions": condition}
+    res = actions.call_contract(url, pr_key, "@1NewParameter",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               "name": name}
     return result
 
-def edit_parameter(url, pr_key, token, id, value="test_edited", condition="true"):
-    data = {"Id": id, "Value": value, "Conditions": condition}
-    result = {"hash": actions.call_contract(url, pr_key, "EditParameter", data, token)}
+
+def edit_parameter(url, pr_key, token, id, value="test_edited",
+                   condition="true", ecosystem=1):
+    data = {"Id": id,
+            "Value": value,
+            "Conditions": condition}
+    res = actions.call_contract(url, pr_key, "@1EditParameter",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def new_menu(url, pr_key, token, name='', value="Item1", condition="true"):
-    if name == '':
+def new_menu(url, pr_key, token, name='', value="Item1",
+             condition="true", ecosystem=1):
+    if not name:
         name = "Menu_" + tools.generate_random_name()
-    data = {"Name": name, "Value": value, "Conditions": condition}
-    result = {"hash": actions.call_contract(url, pr_key, "NewMenu", data, token),
+    data = {"Name": name,
+            "Value": value,
+            "Conditions": condition}
+    res = actions.call_contract(url, pr_key, "@1NewMenu",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               "name": name}
     return result
 
-def edit_menu(url, pr_key, token, id, value="ItemEdited", condition="true"):
-    data = {"Id": id, "Value": value, "Conditions": condition}
-    result = {"hash": actions.call_contract(url, pr_key, "EditMenu", data, token)}
+
+def edit_menu(url, pr_key, token, id, value="ItemEdited",
+              condition="true", ecosystem=1):
+    data = {"Id": id,
+            "Value": value,
+            "Conditions": condition}
+    res = actions.call_contract(url, pr_key, "@1EditMenu",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def append_item(url, pr_key, token, id, value='AppendedItem'):
-    data = {"Id": id, "Value": value}
-    result = {"hash": actions.call_contract(url, pr_key, "AppendedItem", data, token)}
+
+def append_item(url, pr_key, token, id, value='AppendedItem', ecosystem=1):
+    data = {"Id": id,
+            "Value": value}
+    res = actions.call_contract(url, pr_key, "@1AppendedItem",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
     return result
 
-def new_page(url, pr_key, token, name='', value='', app_id=1, condition='true', menu='default_menu'):
-    if name == '':
+
+def new_page(url, pr_key, token, name='', value='', app_id=1,
+             condition='true', menu='default_menu', ecosystem=1):
+    if not name:
         name = "Page_" + tools.generate_random_name() 
-    if value == '':
+    if not value:
         value = "Hello page!"
-    data = {"Name": name, "Value": value, "ApplicationId": app_id,
-            "Conditions": condition, "Menu": menu}
-    result = {"hash": actions.call_contract(url, pr_key, "NewPage", data, token),
+    data = {"Name": name,
+            "Value": value,
+            "ApplicationId": app_id,
+            "Conditions": condition,
+            "Menu": menu}
+    res = actions.call_contract(url, pr_key, "@1NewPage",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res,
               "name": name}
     return result
 
-def edit_page(url, pr_key, token, id, condition='true', menu='default_menu', value=''):
-    if value == '':
-        value = "Good by page!"
-    data_edit = {"Id": id, "Value": value, "Conditions": condition, "Menu": menu}
-    result = {"hash": actions.call_contract(url, pr_key, "EditPage", data, token)}
-    return result
 
+def edit_page(url, pr_key, token, id, condition='true',
+              menu='default_menu', value='', ecosystem=1):
+    if not value:
+        value = "Good by page!"
+    data = {"Id": id,
+            "Value": value,
+            "Conditions": condition,
+            "Menu": menu}
+    res = actions.call_contract(url, pr_key, "@1EditPage",
+                                data, token, ecosystem=ecosystem)
+    result = {"hash": res}
+    return result
 
