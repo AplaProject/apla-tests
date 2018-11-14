2ad5587138c66952c1ecd21ec4546e494dd58b1d
Lena Konkina <lfreze@gmail.com>
2018-11-14 10:33:42 +0600

    Fixed test_bind_wallet

--- test_cost.py
+++ test_cost.py
@@ -46,14 +46,16 @@ class TestCost():
                 TestCost.conf[0]['url'], TestCost.wait, tx, token_creater)
 
     def bind_wallet(self):
-        data_creater = actions.login(
-            self.conf[1]['url'], self.conf[0]['private_key'], 0)
-        token_creater = data_creater['jwtToken']
-        id = actions.get_contract_id(
-            self.conf[1]['url'], 'CostContract', token_creater)
-        tx = contract.bind_wallet(self.conf[1]['url'], self.conf[0]['private_key'],
-                                  token_creater, id)
-        check.is_tx_in_block(self.conf[1]['url'], self.wait, tx, token_creater)
+        data_wallet = actions.login(self.conf[1]['url'], self.keys['key1'], 0)
+        token_creater = data_wallet['jwtToken']
+        id = actions.get_contract_id(self.conf[1]['url'], 'CostContract', token_creater)
+        tx = contract.bind_wallet(self.conf[1]['url'], self.keys['key1'],
+                                  token_creater, id, wallet=data_wallet['address'])
+        block = check.is_tx_in_block(self.conf[1]['url'], self.wait, tx, token_creater)
+        if block > 0:
+            return data_wallet['key_id']
+        else:
+            print('bind is faild')
 
     def unbind_wallet(self):
         data_creater = actions.login(
@@ -79,7 +81,7 @@ class TestCost():
 
     def test_bind_wallet(self):
         if not actions.is_contract_activated(self.conf[1]['url'], 'CostContract', self.token):
-            self.bind_wallet()
+            bind_wallet = self.bind_wallet()
         time.sleep(10)
         wallet_id = actions.get_activated_wallet(self.conf[1]['url'],
                                                  'CostContract', self.token)
@@ -89,15 +91,16 @@ class TestCost():
         data_runner = actions.login(self.conf[1]['url'], self.keys['key2'], 0)
         token_runner = data_runner['jwtToken']
         balance_runner_b = actions.get_balance_by_id(self.conf[1]['url'], self.token,
-                                                     data_runner['key_id'])
+                                                 data_runner['key_id'])
+        balance_contract_owner_b = actions.get_balance_by_id(self.conf[1]['url'], self.token,
+                                                        wallet_id)
         res = actions.call_contract(self.conf[1]['url'], self.keys['key2'],
-                                    'CostContract', {'State': 1}, token_runner)
-        result = actions.tx_status(
-            self.conf[1]['url'], self.wait, res, token_runner)
+                                    'CostContract', {"State": 1}, token_runner)
+        result = actions.tx_status(self.conf[1]['url'], self.wait, res, token_runner)
         time.sleep(10)
+        
         node = db.get_block_gen_node(self.conf[0]['db'], result['blockid'])
-        summ_after = sum(actions.get_user_token_amounts(
-            self.conf[1]['url'], self.token))
+        summ_after = sum(actions.get_user_token_amounts(self.conf[1]['url'], self.token))
         a_node_balance = self.get_node_balances()
         balance_runner_a = actions.get_balance_by_id(self.conf[1]['url'], self.token,
                                                      data_runner['key_id'])
@@ -105,30 +108,34 @@ class TestCost():
                                                              wallet_id)
         node_commission = NODE_COMISSION
         platforma_commission = PLATFORM_COMISSION
-        in_history = self.is_commissions_in_history(node_commission, self.conf[0]['keyID'],
-                                                    platforma_commission, node)
+        in_history = self.is_commissions_in_history(node_commission, wallet_id,
+                                                   platforma_commission, node)
+        bal_own =  int(balance_contract_owner_b) - (node_commission + platforma_commission)
         if node == 0:
+            bal_plat = int(b_node_balance[0]) + node_commission + platforma_commission
             dict_valid = dict(balanceRunner=int(balance_runner_a),
-                              platformBalance=int(a_node_balance[0]),
-                              summ=int(summ_after),
-                              history=in_history)
+                             platformBalance=int(a_node_balance[0]),
+                             balanceOwner=int(balance_contract_owner_a),
+                             summ=int(summ_after),
+                             history=in_history)
             dict_expect = dict(balanceRunner=int(balance_runner_b),
-                               platformBalance=int(a_node_balance[0]),
-                               summ=int(summ_before),
-                               history=True)
+                               balanceOwner = bal_own,
+                              platformBalance=bal_plat,
+                              summ=int(summ_before),
+                              history=True)
         else:
             dict_valid = dict(balanceRunner=int(balance_runner_a),
-                              platformBalance=int(a_node_balance[0]),
-                              nodeBalance=int(a_node_balance[node]),
-                              summ=int(summ_before),
-                              history=in_history)
+                             platformBalance=int(a_node_balance[0]),
+                             nodeBalance=int(a_node_balance[node]),
+                             balanceOwner=int(balance_contract_owner_a),
+                             summ=int(summ_before),
+                             history=in_history)
             dict_expect = dict(balanceRunner=int(balance_runner_b),
-                               platformBalance=int(
-                                   b_node_balance[0]) - int(node_commission),
-                               nodeBalance=int(
-                                   b_node_balance[node]) + int(node_commission),
-                               summ=int(summ_after),
-                               history=True)
+                               balanceOwner = bal_own,
+                              platformBalance=int(b_node_balance[0]) + int(platforma_commission),
+                              nodeBalance=int(b_node_balance[node]) + int(node_commission),
+                              summ=int(summ_after),
+                              history=True)
         self.u.assertDictEqual(dict_valid, dict_expect,
                                'Error in comissions for bind_wallet')
 
@@ -164,11 +171,13 @@ class TestCost():
                                                     platforma_commission, node)
         if node == 0:
             dict_valid = {'balanceRunner': int(balance_runner_a),
-                          'platformBalance': int(a_node_balance[0]), 'summ': int(summ_before),
+                          'platformBalance': int(a_node_balance[0]),
+                          'summ': int(summ_before),
                           'history': in_history}
             dict_expect = {'balanceRunner': int(balance_runner_b) - int(commission),
                            'platformBalance': int(b_node_balance[0]) + int(commission),
-                           'summ': int(summ_after), 'history': True}
+                           'summ': int(summ_after),
+                           'history': True}
         else:
             dict_valid = {'balanceRunner': int(balance_runner_a),
                           'platformBalance': int(a_node_balance[0]),
